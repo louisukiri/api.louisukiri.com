47ce423e89b52d72af6025951b1115da6436153e
Louis Ukiri <lukiri@godaddy.com>
2015-06-22 17:16:57 -0500

    Add CICD model components
    Add CICD model components

---
+++ api.louisukiri.com.Tests/UnitTest1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using cicdDomain;
+using NUnit.Framework;
+using cicdDomain.cicd.domain.service;
+using cicd.domain.cicd.domain.abstracts;
+using Moq;
+
+namespace api.louisukiri.com.Tests
+{
+  [TestFixture]
+  public class cicdserviceTest
+  {
+    private CICDService sut;
+    private Mock<IBuildServer> buildServer;
+
+    [TestFixtureSetUp]
+    public void setup()
+    {
+      buildServer = new Mock<IBuildServer>();
+    }
+    [Test]
+    public void givenIdentifierCallsBuildServerService()
+    {
+      string name = testInfrastructure.random;
+      sut.send(name);
+
+      
+    }
+  }
+}
